a3935f0c45c7e2a1dec37eb8905dad83ad3a7178
leonardos4enz <leonardorsaenz@gmail.com>
2023-04-16 15:07:24 -0600

    Funciones nivel medio

---
+++ app/src/main/java/com/example/fundamentos_kotlin/Funciones.kt
@@ -0,0 +1,14 @@
+package com.example.fundamentos_kotlin
+
+fun main(){
+    newTopic("Funciones nivel medio")
+    sayHello()
+}
+
+
+//Función privada
+
+//Unit = VOID
+private fun sayHello(): Unit {
+    println("Hello")
+}
