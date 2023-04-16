46cec226635d02475f58c745045adc7311ae5c27
leonardos4enz <leonardorsaenz@gmail.com>
2023-04-16 15:16:02 -0600

    Retorno de datos

--- app/src/main/java/com/example/fundamentos_kotlin/Funciones.kt
+++ app/src/main/java/com/example/fundamentos_kotlin/Funciones.kt
@@ -3,6 +3,13 @@ package com.example.fundamentos_kotlin
 fun main(){
     newTopic("Funciones nivel medio")
     sayHello()
+
+    newTopic("Retorno de datos")
+    val a = 2
+    val b = 3
+    println("$a + $b = ${sum(a,b)}")
+    println("$a - $b = ${sub(a,b)}")
+
 }
 
 
@@ -12,3 +19,9 @@ fun main(){
 private fun sayHello(): Unit {
     println("Hello")
 }
+
+fun sum(a: Int, b: Int): Int{
+    return a + b
+}
+
+fun sub(a: Int, b: Int) = a - b
