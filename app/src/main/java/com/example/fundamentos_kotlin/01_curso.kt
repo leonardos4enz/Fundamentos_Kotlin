ce026d5e42686b729c49fe23e8f28aaad3296e8d
leonardos4enz <leonardorsaenz@gmail.com>
2023-04-16 14:57:28 -0600

    Variables y constantes

--- app/src/main/java/com/example/fundamentos_kotlin/01_curso.kt
+++ app/src/main/java/com/example/fundamentos_kotlin/01_curso.kt
@@ -1,7 +1,13 @@
 package com.example.fundamentos_kotlin
 
 fun main(){
-    newTopic("Hola Kotlin")
+    newTopic("Variables y constantes")
+
+    val a = 2 //Constante
+    var b = 3 //Variable
+    println("a = $a\nb = $b")
+
+
 }
 
 fun newTopic(topic: String){
